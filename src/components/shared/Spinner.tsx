1526ccfda137ba1277b49d4aed53ebb657bd28d3
YaroslavMelnyk39 <143670684+YaroslavMelnyk39@users.noreply.github.com>
2023-08-31 06:46:36 +0300

    add using darth icon for spinner

--- src/components/shared/Spinner.tsx
+++ src/components/shared/Spinner.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import DarthIcon from './darth.svg';
 
 const Spinner: React.FC = () => (
   <div className="flex justify-center items-center">
-    <div className="animate-spin rounded-full h-10 w-10 border-t-2 border-b-2 border-gray-900"></div>
+    <div className="animate-spin rounded-full h-10 w-10 border-t-2 border-b-2 border-gray-900">
+        <img src={DarthIcon} alt="darth icon" />
+    </div>
   </div>
 );
 
